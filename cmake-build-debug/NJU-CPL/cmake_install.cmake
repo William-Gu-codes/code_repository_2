32c1ff0347fcf529f3edc33c85d11f4506b0da5c
Gu <2463969626@qq.com>
2023-02-04 07:51:22 +0800

    3

--- cmake-build-debug/NJU-CPL/cmake_install.cmake
+++ cmake-build-debug/NJU-CPL/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/GDC/Desktop/code_repositories/code_repository_2
+# Install script for directory: C:/Users/GDC/Desktop/code_repositories/code_repository_2/NJU-CPL
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -37,19 +37,3 @@ if(NOT DEFINED CMAKE_OBJDUMP)
   set(CMAKE_OBJDUMP "C:/Users/GDC/Desktop/CLion 2022.2.1/bin/mingw/bin/objdump.exe")
 endif()
 
-if(NOT CMAKE_INSTALL_LOCAL_ONLY)
-  # Include the install script for each subdirectory.
-  include("C:/Users/GDC/Desktop/code_repositories/code_repository_2/cmake-build-debug/NJU-CPL/cmake_install.cmake")
-
-endif()
-
-if(CMAKE_INSTALL_COMPONENT)
-  set(CMAKE_INSTALL_MANIFEST "install_manifest_${CMAKE_INSTALL_COMPONENT}.txt")
-else()
-  set(CMAKE_INSTALL_MANIFEST "install_manifest.txt")
-endif()
-
-string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
-       "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/GDC/Desktop/code_repositories/code_repository_2/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
-     "${CMAKE_INSTALL_MANIFEST_CONTENT}")
